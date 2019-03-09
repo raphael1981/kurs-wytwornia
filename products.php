1cd0b9fe4de8549637a691568917865eac045bf4
unknown <erafal.majewski@gmail.com>
2019-03-09 16:45:30 +0100

    un comment

--- products.php
+++ products.php
@@ -14,35 +14,35 @@ $db = new MysqliDb(
 );
 
 
-//for($i=0;$i<20;$i++){
-//
-//
-//    $array = [
-//        'name'=>$faker->unique()->city,
-//        'created_at'=>date('Y-m-d H:i:s')
-//    ];
-//
-//    $id = $db->insert('categories', $array);
-//
-//    echo $id;
-//
-//}
-//
-//$cCount = count($db->get('categories'));
-//
-//for($i=0;$i<200;$i++){
-//
-//    $array = [
-//        'category_id'=>$faker->numberBetween(1,$cCount),
-//        'name'=>$faker->unique()->city,
-//        'description'=>$faker->sentence,
-//        'price'=>$faker->randomFloat(2,1,10000),
-//        'status'=>$faker->numberBetween(0,1),
-//        'created_at'=>date('Y-m-d H:i:s')
-//    ];
-//
-//    $id = $db->insert('products', $array);
-//
-//    echo $id;
-//
-//}
+for($i=0;$i<20;$i++){
+
+
+    $array = [
+        'name'=>$faker->unique()->city,
+        'created_at'=>date('Y-m-d H:i:s')
+    ];
+
+    $id = $db->insert('categories', $array);
+
+    echo $id;
+
+}
+
+$cCount = count($db->get('categories'));
+
+for($i=0;$i<200;$i++){
+
+    $array = [
+        'category_id'=>$faker->numberBetween(1,$cCount),
+        'name'=>$faker->unique()->city,
+        'description'=>$faker->sentence,
+        'price'=>$faker->randomFloat(2,1,10000),
+        'status'=>$faker->numberBetween(0,1),
+        'created_at'=>date('Y-m-d H:i:s')
+    ];
+
+    $id = $db->insert('products', $array);
+
+    echo $id;
+
+}
